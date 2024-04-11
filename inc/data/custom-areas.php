294c6f7ecb926cbca49ac95f0236ae7349af9686
davidbp845 <david.bp845@gmail.com>
2024-04-11 13:19:19 +0200

    added some more templates and custom css

--- inc/data/custom-areas.php
+++ inc/data/custom-areas.php
@@ -6,7 +6,7 @@ $areas = [
         'area_tag' => 'div',
         'label' => __( 'Topbar', 'dualtone' ),
         'description' => __( 'Area on top of the sidebar content columns', 'dualtone' ),
-        'icon' => 'header'
+        'icon' => 'sidebar'
     ],
     [
         'area' => 'sidebar',
